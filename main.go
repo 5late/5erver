a1a3d880dda462c8015a6710588962cc3cb2e295
5late <christianrfernandes5@gmail.com>
2021-04-25 18:46:13 -0400

    golang upload to backend!

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+var templates = template.Must(template.ParseFiles("public/upload.html"))
+
+func displayHTML(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page+".html", data)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Uploading File")
+	// Maximum upload of 10 MB files
+	r.ParseMultipartForm(10 << 20)
+
+	// Get handler for filename, size and headers
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	f, err := os.Create(handler.Filename)
+	defer f.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(f, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func setupRoutes(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		displayHTML(w, "upload", nil)
+	case "POST":
+		uploadFile(w, r)
+	}
+}
+
+func main() {
+	fmt.Println("Hello World!")
+	http.HandleFunc("/upload", setupRoutes)
+	http.ListenAndServe(":1337", nil)
+}
